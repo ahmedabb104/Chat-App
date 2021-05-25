ed7238302e213936c9b6c99846f3d1cab863da55
Ahmed Abbas <ahmedabb101@gmail.com>
2021-05-25 17:21:37 +0300

    connecting database

--- server.js
+++ server.js
@@ -2,15 +2,24 @@
 require('dotenv').config();
 
 const express = require('express');
+const myDB = require('./connection');
+
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-	res.render(process.cwd() + '/views/index.pug')
-})
+
+// Connecting to DB once server is started, keeping persistent connection for full lifecycle of app
+myDB(async (client) => {
+	const myDatabase = await client.db('userDatabase').collection('users');
+
+	app.route('/').get((req, res) => {
+		res.render(process.cwd() + '/views/index.pug')
+	})
+
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
